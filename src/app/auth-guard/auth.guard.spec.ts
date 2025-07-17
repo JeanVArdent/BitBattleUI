b4c9dea7b97e2b00345ede5d3697bbd5e1e3f477
brandongasser <bgasser12@gmail.com>
2025-07-17 00:16:04 -0500

    chore: change coverage threshold

--- src/app/auth-guard/auth.guard.spec.ts
+++ src/app/auth-guard/auth.guard.spec.ts
@@ -18,21 +18,21 @@ describe('AuthGuard', () => {
     expect(guard).toBeTruthy();
   });
 
-  describe('canActivate', () => {
-    // THIS TEST IS TEMPORARY AND SHOULD BE REMOVED WHEN FUNCTIONALITY IS ADDED
-    it('should return true in all cases', async () => {
-      const response = guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+  // describe('canActivate', () => {
+  //   // THIS TEST IS TEMPORARY AND SHOULD BE REMOVED WHEN FUNCTIONALITY IS ADDED
+  //   it('should return true in all cases', async () => {
+  //     const response = guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
 
-      let result: GuardResult | undefined;
-      if (response instanceof Observable) {
-        result = await response.toPromise();
-      } else if (response instanceof Promise) {
-        result = await response;
-      } else {
-        result = response;
-      }
+  //     let result: GuardResult | undefined;
+  //     if (response instanceof Observable) {
+  //       result = await response.toPromise();
+  //     } else if (response instanceof Promise) {
+  //       result = await response;
+  //     } else {
+  //       result = response;
+  //     }
 
-      expect(result?.valueOf()).toBeTrue();
-    });
-  });
+  //     expect(result?.valueOf()).toBeTrue();
+  //   });
+  // });
 });
