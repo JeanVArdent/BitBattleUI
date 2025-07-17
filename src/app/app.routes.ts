41a0a9f170196bf013380e001278a3dd2ab05601
Brandon Gasser <34321725+brandongasser@users.noreply.github.com>
2025-07-16 23:39:54 -0500

    task: Feature/#33 Create components and services (#4)
    * task: Stub out services and components
    
    * task: Add access denied page
    
    * task: add missing components and services
    
    * task: add test coverage script

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,69 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { DocsComponent } from './docs/docs.component';
+import { CompetitorsComponent } from './admin/competitors/competitors.component';
+import { AuthGuard } from './auth-guard/auth.guard';
+import { StreamManagementComponent } from './admin/stream-management/stream-management.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { SubmissionComponent } from './submission/submission.component';
+import { VisualizationComponent } from './visualization/visualization.component';
+import { TestingComponent } from './testing/testing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'docs',
+    component: DocsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'submission',
+    component: SubmissionComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'visualizer',
+    component: VisualizationComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'test',
+    component: TestingComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin/competitors',
+    component: CompetitorsComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin/stream',
+    component: StreamManagementComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+];
